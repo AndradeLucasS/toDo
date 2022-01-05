8a0f33c6a7b9840c41bba78bc6e8272f3f0b322f
AndradeLucasS <95777438+AndradeLucasS@users.noreply.github.com>
2022-01-05 19:21:11 -0300

    v1.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,20 @@
 let Todo = {
+    input: null,
+    newEl: null,
+    btn: document.querySelector(".button"),
+    list: document.querySelector(".list"),
+
     startApp(){
+        this.btn.addEventListener("click", this.btnEvent);
         console.log("Your application started successfully...");
     },
+
+    btnEvent(){
+        console.log("Foi?");
+        this.newEl = document.createElement("input");
+        this.newEl.className = "list-item";
+        Todo.list.appendChild(this.newEl);
+    }
 }
 
-App.startApp();
+Todo.startApp();
